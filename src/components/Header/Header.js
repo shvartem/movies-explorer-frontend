3e9a282fcc58f8141430796d62a4db452baa5501
shvartem <shvartem422@yandex.ru>
2021-12-13 13:45:13 +0500

    Рефактор Navigation с адаптивом + BurgerButton

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,44 @@
 import './Header.css';
+import { Link } from 'react-router-dom';
 import logo from '../../images/logo.svg';
 import Navigation from '../Navigation/Navigation';
+import BurgerButton from '../BurgerButton/BurgerButton';
+// import accountButton from '../../images/account-button.svg';
 
 function Header() {
+  // const currentUser = true;
+
   return (
     <section className="header">
-      <div className="section__container section__container_wide header__container">
-        <div className="header__left">
+      <div className="section__container header__container">
+        <Link to="/" className="header__link header__link_first-of-type">
           <img src={logo} alt="Логотип сервиса Диплом" className="header__logo" />
-          <button type="button" className="header__button">Фильмы</button>
-          <button type="button" className="header__button">Сохранённые фильмы</button>
-        </div>
-        <div className="header__right">
-          <button type="button" className="header__button">Регистрация</button>
-          <button type="button" className="header__button header__button_last-of-type">Войти</button>
-        </div>
+        </Link>
+        <BurgerButton />
+        <Navigation />
+
+        {/* <div className="header__links"> */}
+        {/*  <div className="header__left"> */}
+        {/*    {currentUser &&
+        <Link to="/movies" className="header__link">Фильмы</Link>} */}
+        {/*    {currentUser &&
+        <Link to="/saved-movies" className="header__link">Сохранённые фильмы</Link>} */}
+        {/*  </div> */}
+        {/*  <div className="header__right"> */}
+        {/*    {!currentUser &&
+        <Link to="/signup" className="header__link">Регистрация</Link>} */}
+        {/*    {!currentUser &&
+        <Link to="/signin" className="header__link header__link_last-of-type">Войти</Link>} */}
+
+        {/*    {currentUser && ( */}
+        {/*    <Link to="/profile" className="navigation__account-link"> */}
+        {/*      <span className="navigation__account-text">Аккаунт</span> */}
+        {/*      <img src={accountButton} alt="" className="navigation__account-image" /> */}
+        {/*    </Link> */}
+        {/*    )} */}
+        {/*  </div> */}
+        {/* </div> */}
       </div>
-      <Navigation />
     </section>
   );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,6 +4,8 @@ import closeButton from '../../images/close-button.svg';
 import accountButton from '../../images/account-button.svg';
 
 function Navigation() {
+  const currentUser = true;
+
   return (
     <div className="navigation">
       <div className="navigation__container">
@@ -11,15 +13,23 @@ function Navigation() {
           <img src={closeButton} alt="" className="navigation__close-image" />
         </button>
         <nav className="navigation__nav">
-          <NavLink exact to="/" activeClassName="navigation__link_active" className="navigation__link">Главная</NavLink>
-          <NavLink to="/movies" activeClassName="navigation__link_active" className="navigation__link">Фильмы</NavLink>
-          <NavLink to="/saved-movies" activeClassName="navigation__link_active" className="navigation__link">Сохранённые фильмы</NavLink>
+          <NavLink exact to="/" activeClassName="navigation__link_active" className="navigation__link navigation__link_type_main">Главная</NavLink>
+          {currentUser && <NavLink to="/movies" activeClassName="navigation__link_active" className="navigation__link">Фильмы</NavLink>}
+          {currentUser && <NavLink to="/saved-movies" activeClassName="navigation__link_active" className="navigation__link">Сохранённые фильмы</NavLink>}
         </nav>
+
         <div className="navigation__account">
-          <Link to="/profile" className="navigation__account-link">
-            <span className="navigation__account-text">Аккаунт</span>
-            <img src={accountButton} alt="" className="navigation__account-image" />
-          </Link>
+          {currentUser && (
+            <>
+              <NavLink to="/profile" activeClassName="navigation__link_active navigation__link_type_active-account" className="navigation__link navigation__link_type_account">
+                Аккаунт
+              </NavLink>
+              <Link to="/profile" className="navigation__account-image">
+                <img src={accountButton} alt="" className="navigation__account-image" />
+              </Link>
+            </>
+          )}
+
         </div>
       </div>
     </div>
